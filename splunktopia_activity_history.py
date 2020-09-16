458912581bb2dc4f77ad701090494a0de6bdca31
Mike Walker <noisufnoc@gmail.com>
2020-09-16 16:58:27 -0400

    error handling if you pass an invalid arg to the activity history script

--- splunktopia_activity_history.py
+++ splunktopia_activity_history.py
@@ -15,11 +15,15 @@ profile = client.get_profile(player_id)
 activity = client.get_activity(player_id)
 
 if len(sys.argv) == 5:
-    if int(sys.argv[4]) < 20:
-        activity_list = activity.list(limit=sys.argv[4])
-        for i in activity_list:
-            print(i)
-    else:
-        print(activity.list())
+    try:
+        if int(sys.argv[4]) < 20:
+            activity_list = activity.list(limit=sys.argv[4])
+            for i in activity_list:
+                print(i)
+        else:
+            print(activity.list())
+    except:
+        print("Got invalid arg. Exiting")
+        pass
 else:
     print(profile.latest_activity)
