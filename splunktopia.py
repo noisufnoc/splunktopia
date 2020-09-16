68328c74a278c3111169131b7758792d3b5c8a8e
Mike Walker <noisufnoc@gmail.com>
2020-09-16 10:35:25 -0400

    just getting started with argv for username, passwd, player_id

---
+++ splunktopia.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+__author__ = 'Mike Walker'
+
+import sys
+import zwift
+
+username = sys.argv[1]
+password = sys.argv[2]
+player_id = sys.argv[3]
+
+print('username is ', username)
+print('password is ', password)
+print('player_id is ', player_id)
