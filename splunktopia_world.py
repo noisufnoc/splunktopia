0d15fd11b07e1d154c3e24fd7208c3fd3f45e6f0
Mike Walker <noisufnoc@gmail.com>
2020-09-17 15:38:50 -0400

    i found more properties to collect, collecting them

--- splunktopia_world.py
+++ splunktopia_world.py
@@ -30,7 +30,29 @@ if profile.latest_activity['profile']['riding']:
         'power': world.player_status(player_id).power,
         'speed': world.player_status(player_id).speed,
         'altitude': world.player_status(player_id).altitude,
-        'distance': world.player_status(player_id).distance
+        'distance': world.player_status(player_id).distance,
+        'heading': world.player_status(player_id).heading,
+        'lean': world.player_status(player_id).lean,
+        'f19': world.player_status(player_id).f19,
+        'f20': world.player_status(player_id).f20,
+        'x': world.player_status(player_id).x,
+        'y': world.player_status(player_id).y,
+        'justWatching': world.player_status(player_id).justWatching,
+        'watchingRiderId': world.player_status(player_id).watchingRiderId,
+        'roadTime': world.player_status(player_id).roadTime,
+        'roadPosition': world.player_status(player_id).roadPosition,
+        'course': world.player_status(player_id).course,
+        'has_aero_boost': world.player_status(player_id).has_aero_boost,
+        'has_draft_boost': world.player_status(player_id).has_draft_boost,
+        'has_feather_boost': world.player_status(player_id).has_feather_boost,
+        'is_forward': world.player_status(player_id).is_forward,
+        'is_turning': world.player_status(player_id).is_turning,
+        'power_up': world.player_status(player_id).power_up,
+        'ride_ons': world.player_status(player_id).ride_ons,
+        'road_direction': world.player_status(player_id).road_direction,
+        'road_id': world.player_status(player_id).road_id,
+        'turn_signal': world.player_status(player_id).turn_signal,
+        'worldTime': world.player_status(player_id).worldTime
     })
 
     print(data)
